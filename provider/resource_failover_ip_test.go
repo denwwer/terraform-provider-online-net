d934b81f60dfc217a73d1678c514909f74246817
Maartje Eyskens <maartje@eyskens.me>
2018-10-02 20:52:24 +0200

    Add online_failover_ip acceptance tests
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- provider/resource_failover_ip_test.go
+++ provider/resource_failover_ip_test.go
@@ -2,7 +2,9 @@ package provider
 
 import (
 	"testing"
+	"time"
 
+	"github.com/hashicorp/terraform/terraform"
 	"github.com/src-d/terraform-provider-online-net/online"
 
 	"github.com/hashicorp/terraform/helper/resource"
@@ -52,6 +54,63 @@ func TestResourceFailoverIP(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("online_failover_ip.test", "ip", "127.0.0.1"),
 					resource.TestCheckResourceAttr("online_failover_ip.test", "mac", "ma:ac:te:st"),
+				),
+			},
+		},
+	})
+}
+
+func TestResourceFailoverIPAcceptance(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers:  testAccProviders,
+		IsUnitTest: false,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() {
+					// we are modifying routing tables here
+					// online.net will error if we change these too quickly
+					time.Sleep(10 * time.Second)
+				},
+				ImportStateVerify: false,
+				Config: `
+						resource "online_failover_ip" "test" {
+			 				"ip" = "51.158.20.2"
+							"destination_server_id" = 105711
+						}
+					`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("online_failover_ip.test", "ip", "51.158.20.2"),
+					func(s *terraform.State) error {
+						// we are modifying routing tables here
+						// online.net will error if we change these too quickly
+						time.Sleep(10 * time.Second)
+						return nil
+					},
+				),
+			},
+			{
+				PreConfig: func() {
+					// we are modifying routing tables here
+					// online.net will error if we change these too quickly
+					time.Sleep(10 * time.Second)
+				},
+				ImportStateVerify: false,
+				Config: `
+				resource "online_failover_ip" "test" {
+					 "ip" = "51.158.20.2"
+					 "destination_server_id" = 105711
+					 "generate_mac" = true
+				}
+			`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					func(s *terraform.State) error {
+						// we are modifying routing tables here
+						// online.net will error if we change these too quickly
+						time.Sleep(10 * time.Second)
+						return nil
+					},
+					resource.TestCheckResourceAttr("online_failover_ip.test", "ip", "51.158.20.2"),
+					resource.TestCheckResourceAttrSet("online_failover_ip.test", "mac"),
 				),
 			},
 		},
